df08b8007cfe32239969bec0c58275053d9daf87
kccthays <kerollynthayssa@gmail.com>
2026-07-15 00:33:43 -0400

    Primeira estrutura do Travel AI Agent

---
+++ app/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("===================================")
+    print("   Bem-vinda ao Travel AI Agent!   ")
+    print("===================================")
+    print("Projeto criado por Thay 🚀")
+
+if __name__ == "__main__":
+    main()
