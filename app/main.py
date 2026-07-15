6c823e0518f7f9545ecbf1339ca819349f865abf
Thayssa <kerollynthayssa@gmail.com>
2026-07-15 17:32:31 +0000

    refactor: move flight creation to flight service

--- app/main.py
+++ app/main.py
@@ -1,27 +1,25 @@
-from models.flight import Flight
-
+from services.flight_service import (
+    find_cheapest_flight,
+    find_flights
+)
 
 def main():
+    origin = input("Origem: ")
+    destination = input("Destino: ")
+    departure_date = input("Data da viagem: ")
 
-    flight = Flight(
-        origin="Campo Grande",
-        destination="São Paulo",
-        departure_date="10/10/2026",
-        departure_time="09:30",
-        arrival_time="11:15",
-        airline="LATAM",
-        price=1290.90,
-        baggage="1 bagagem",
-        stops=0,
-        duration="1h45",
-        purchase_link="https://latam.com"
+    flights = find_flights(
+        origin,
+        destination,
+        departure_date
     )
 
-    print("===== PRIMEIRO VOO =====")
-    print(f"Origem: {flight.origin}")
-    print(f"Destino: {flight.destination}")
-    print(f"Preço: R$ {flight.price}")
-    print(f"Companhia: {flight.airline}")
+    cheapest_flight = find_cheapest_flight(flights)
+
+    print("===== MELHOR VOO =====")
+    print(f"Companhia: {cheapest_flight.airline}")
+    print(f"Preço: R$ {cheapest_flight.price}")
+    print(f"Quantidade de voos encontrados: {len(flights)}")
 
 
 if __name__ == "__main__":
